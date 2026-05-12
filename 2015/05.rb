ac486604897028c632ba0ec73f3171fae9948c27
Highlander Paiva <highlander.paiva@rdstation.com>
2026-05-12 16:31:14 -0300

    feat: add 2015 day 05 solution

--- 2015/05.rb
+++ 2015/05.rb
@@ -1,12 +1,49 @@
 require_relative "../aoc"
 
-# example <<~INPUT, part1: 0
-# INPUT
+VOWELS = "aeiou"
+DOUBLE = /(.)\1/
+FORBIDDEN = /ab|cd|pq|xy/
+TWICE = /(..).*\1/
+SANDWICH = /(.).\1/
 
-def parsed
-  @parsed ||= input.lines(chomp: true)
-end
+example <<~INPUT, part1: 1
+  ugknbfddgicrmopn
+INPUT
 
-def part1
-  raise "part1 not implemented"
-end
+example <<~INPUT, part1: 1
+  aaa
+INPUT
+
+example <<~INPUT, part1: 0
+  jchzalrnumimnmhp
+INPUT
+
+example <<~INPUT, part1: 0
+  haegwjzuvuyypxyu
+INPUT
+
+example <<~INPUT, part1: 0
+  dvszwmarrgswjxmb
+INPUT
+
+example <<~INPUT, part2: 1
+  qjhvhtzxzqqjkmpb
+INPUT
+
+example <<~INPUT, part2: 1
+  xxyxx
+INPUT
+
+example <<~INPUT, part2: 0
+  uurcxstgmygtbstg
+INPUT
+
+example <<~INPUT, part2: 0
+  ieodomkazucvgmuy
+INPUT
+
+def parsed = @parsed ||= input.lines(chomp: true)
+
+def part1 = parsed.count { it.count(VOWELS) >= 3 && it.match?(DOUBLE) && !it.match?(FORBIDDEN) }
+
+def part2 = parsed.count { it.match?(TWICE) && it.match?(SANDWICH) }
